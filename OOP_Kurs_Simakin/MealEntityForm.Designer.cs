165fc299ec65d7be0ba7d6219c30d128dced7708
Simakin Konstantin <simakinkonstantin@gmail.com>
2024-05-12 14:58:30 +0300

    feature : дизайн для MealFormEntity

--- OOP_Kurs_Simakin/MealEntityForm.Designer.cs
+++ OOP_Kurs_Simakin/MealEntityForm.Designer.cs
@@ -50,8 +50,10 @@
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label1.AutoSize = true;
-            label1.Location = new Point(46, 62);
+            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(267, 39);
             label1.Name = "label1";
             label1.Size = new Size(77, 20);
             label1.TabIndex = 0;
@@ -59,129 +61,152 @@
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label2.AutoSize = true;
-            label2.Location = new Point(48, 115);
+            label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(267, 93);
             label2.Name = "label2";
-            label2.Size = new Size(52, 20);
+            label2.Size = new Size(53, 20);
             label2.TabIndex = 1;
             label2.Text = "Масса";
             // 
             // label3
             // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label3.AutoSize = true;
-            label3.Location = new Point(48, 170);
+            label3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(267, 144);
             label3.Name = "label3";
-            label3.Size = new Size(92, 20);
+            label3.Size = new Size(94, 20);
             label3.TabIndex = 2;
             label3.Text = "Кол-во ккал";
             // 
             // label4
             // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label4.AutoSize = true;
-            label4.Location = new Point(48, 226);
+            label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(267, 193);
             label4.Name = "label4";
-            label4.Size = new Size(45, 20);
+            label4.Size = new Size(46, 20);
             label4.TabIndex = 3;
             label4.Text = "Цена";
             label4.TextAlign = ContentAlignment.TopCenter;
             // 
             // label5
             // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label5.AutoSize = true;
-            label5.Location = new Point(48, 275);
+            label5.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(267, 239);
             label5.Name = "label5";
-            label5.Size = new Size(78, 20);
+            label5.Size = new Size(82, 20);
             label5.TabIndex = 4;
             label5.Text = "Вид кухни";
             // 
             // label6
             // 
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label6.AutoSize = true;
-            label6.Location = new Point(46, 316);
+            label6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(267, 283);
             label6.Name = "label6";
-            label6.Size = new Size(84, 20);
+            label6.Size = new Size(85, 20);
             label6.TabIndex = 5;
             label6.Text = "Тип блюда";
             // 
             // EditedMealName
             // 
-            EditedMealName.Location = new Point(152, 61);
+            EditedMealName.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            EditedMealName.Location = new Point(362, 32);
             EditedMealName.Name = "EditedMealName";
-            EditedMealName.Size = new Size(125, 27);
+            EditedMealName.Size = new Size(136, 27);
             EditedMealName.TabIndex = 6;
             // 
             // EditedMealWeight
             // 
-            EditedMealWeight.Location = new Point(143, 113);
+            EditedMealWeight.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            EditedMealWeight.Location = new Point(362, 86);
             EditedMealWeight.Name = "EditedMealWeight";
-            EditedMealWeight.Size = new Size(150, 27);
+            EditedMealWeight.Size = new Size(136, 27);
             EditedMealWeight.TabIndex = 7;
             // 
             // EditedMealKcal
             // 
-            EditedMealKcal.Location = new Point(151, 174);
+            EditedMealKcal.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            EditedMealKcal.Location = new Point(362, 137);
             EditedMealKcal.Name = "EditedMealKcal";
-            EditedMealKcal.Size = new Size(150, 27);
+            EditedMealKcal.Size = new Size(136, 27);
             EditedMealKcal.TabIndex = 8;
             // 
             // EditedMealPrice
             // 
-            EditedMealPrice.Location = new Point(138, 229);
+            EditedMealPrice.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            EditedMealPrice.Location = new Point(362, 186);
             EditedMealPrice.Name = "EditedMealPrice";
-            EditedMealPrice.Size = new Size(150, 27);
+            EditedMealPrice.Size = new Size(136, 27);
             EditedMealPrice.TabIndex = 9;
             // 
             // EditedMealCuisines
             // 
+            EditedMealCuisines.Anchor = AnchorStyles.Left | AnchorStyles.Right;
             EditedMealCuisines.FormattingEnabled = true;
-            EditedMealCuisines.Location = new Point(137, 275);
+            EditedMealCuisines.Location = new Point(362, 231);
             EditedMealCuisines.Name = "EditedMealCuisines";
-            EditedMealCuisines.Size = new Size(151, 28);
+            EditedMealCuisines.Size = new Size(136, 28);
             EditedMealCuisines.TabIndex = 10;
             // 
             // EditedMealCategories
             // 
+            EditedMealCategories.Anchor = AnchorStyles.Left | AnchorStyles.Right;
             EditedMealCategories.FormattingEnabled = true;
-            EditedMealCategories.Location = new Point(136, 316);
+            EditedMealCategories.Location = new Point(362, 275);
             EditedMealCategories.Name = "EditedMealCategories";
-            EditedMealCategories.Size = new Size(151, 28);
+            EditedMealCategories.Size = new Size(136, 28);
             EditedMealCategories.TabIndex = 11;
             // 
             // ApplyChanges
             // 
-            ApplyChanges.Location = new Point(84, 392);
+            ApplyChanges.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            ApplyChanges.BackColor = SystemColors.Info;
+            ApplyChanges.Location = new Point(362, 392);
             ApplyChanges.Name = "ApplyChanges";
-            ApplyChanges.Size = new Size(94, 29);
+            ApplyChanges.Size = new Size(111, 29);
             ApplyChanges.TabIndex = 12;
             ApplyChanges.Text = "Сохранить изменения";
-            ApplyChanges.UseVisualStyleBackColor = true;
+            ApplyChanges.UseVisualStyleBackColor = false;
             ApplyChanges.Click += ApplyChanges_Click;
             // 
             // ExitMealEntityForm
             // 
-            ExitMealEntityForm.Location = new Point(682, 392);
+            ExitMealEntityForm.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            ExitMealEntityForm.BackColor = SystemColors.Info;
+            ExitMealEntityForm.Location = new Point(592, 392);
             ExitMealEntityForm.Name = "ExitMealEntityForm";
-            ExitMealEntityForm.Size = new Size(94, 29);
+            ExitMealEntityForm.Size = new Size(111, 29);
             ExitMealEntityForm.TabIndex = 13;
             ExitMealEntityForm.Text = "Выход";
-            ExitMealEntityForm.UseVisualStyleBackColor = true;
+            ExitMealEntityForm.UseVisualStyleBackColor = false;
             ExitMealEntityForm.Click += ExitMealEntityForm_Click;
             // 
             // Delete
             // 
-            Delete.Location = new Point(364, 392);
+            Delete.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            Delete.BackColor = SystemColors.Info;
+            Delete.Location = new Point(132, 392);
             Delete.Name = "Delete";
-            Delete.Size = new Size(94, 29);
+            Delete.Size = new Size(111, 29);
             Delete.TabIndex = 14;
             Delete.Text = "Удалить";
-            Delete.UseVisualStyleBackColor = true;
+            Delete.UseVisualStyleBackColor = false;
             Delete.Click += Delete_Click;
             // 
             // MealEntityForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.GradientActiveCaption;
+            ClientSize = new Size(817, 450);
             Controls.Add(Delete);
             Controls.Add(ExitMealEntityForm);
             Controls.Add(ApplyChanges);
@@ -197,6 +222,8 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "MealEntityForm";
             Text = "MealEntityForm";
             Load += MealEntityForm_Load;
